f792716209a2d40ad8d8276425ad61efa43799f3
codyThompson <cody.thompson22@gmail.com>
2015-01-11 22:51:20 -0800

    Physics - added test for RectangleLocation.getPath

--- engine/physics_test/RectangleLocation_test.cs
+++ engine/physics_test/RectangleLocation_test.cs
@@ -6,5 +6,159 @@ using Microsoft.Xna.Framework;
 
 namespace flatverse.physics.test
 {
+    [TestClass]
+    public class RectangleLocation_Test
+    {
+        [TestMethod]
+        public void getPath_test()
+        {
+            Vector2 pos = new Vector2(224.5f, 442.25f);
+            Vector2 delt = new Vector2(300, 600);
+            Vector2 dims = new Vector2(200, 155);
+            Dictionary<string, object>[] testRects = starTestRects(pos, delt, dims.X, dims.Y);
 
+            // moved down
+            FVRectangle orig = testRects[0]["rect"] as FVRectangle;
+            FVRectangle moved = testRects[0]["movedRect"] as FVRectangle;
+            Polygon expectedPath = new Polygon(new Vector2[] {
+                orig.topLeft(),
+                orig.topRight(),
+                moved.bottomRight(),
+                moved.bottomLeft()
+            });
+            Polygon actualPath = (testRects[0]["loc"] as RectangleLocation).getPath(1);
+            Utils.AssertPolygonsAreEqual(expectedPath, actualPath);
+
+            // moved down and right
+            orig = testRects[1]["rect"] as FVRectangle;
+            moved = testRects[1]["movedRect"] as FVRectangle;
+            expectedPath = new Polygon(new Vector2[] {
+                orig.topLeft(),
+                orig.topRight(),
+                moved.topRight(),
+                moved.bottomRight(),
+                moved.bottomLeft(),
+                orig.bottomLeft()
+            });
+            actualPath = (testRects[1]["loc"] as RectangleLocation).getPath(1);
+            Utils.AssertPolygonsAreEqual(expectedPath, actualPath);
+
+            // moved right
+            orig = testRects[2]["rect"] as FVRectangle;
+            moved = testRects[2]["movedRect"] as FVRectangle;
+            expectedPath = new Polygon(new Vector2[] {
+                orig.topLeft(),
+                moved.topRight(),
+                moved.bottomRight(),
+                orig.bottomLeft()
+            });
+            actualPath = (testRects[2]["loc"] as RectangleLocation).getPath(1);
+            Utils.AssertPolygonsAreEqual(expectedPath, actualPath);
+
+            // moved up and right
+            orig = testRects[3]["rect"] as FVRectangle;
+            moved = testRects[3]["movedRect"] as FVRectangle;
+            expectedPath = new Polygon(new Vector2[] {
+                orig.topLeft(),
+                moved.topLeft(),
+                moved.topRight(),
+                moved.bottomRight(),
+                orig.bottomRight(),
+                orig.bottomLeft()
+            });
+            actualPath = (testRects[3]["loc"] as RectangleLocation).getPath(1);
+            Utils.AssertPolygonsAreEqual(expectedPath, actualPath);
+
+            // moved up
+            orig = testRects[4]["rect"] as FVRectangle;
+            moved = testRects[4]["movedRect"] as FVRectangle;
+            expectedPath = new Polygon(new Vector2[] {
+                moved.topLeft(),
+                moved.topRight(),
+                orig.bottomRight(),
+                orig.bottomLeft()
+            });
+            actualPath = (testRects[4]["loc"] as RectangleLocation).getPath(1);
+            Utils.AssertPolygonsAreEqual(expectedPath, actualPath);
+
+            // moved up and left
+            orig = testRects[5]["rect"] as FVRectangle;
+            moved = testRects[5]["movedRect"] as FVRectangle;
+            expectedPath = new Polygon(new Vector2[] {
+                moved.topLeft(),
+                moved.topRight(),
+                orig.topRight(),
+                orig.bottomRight(),
+                orig.bottomLeft(),
+                moved.bottomLeft()
+            });
+            actualPath = (testRects[5]["loc"] as RectangleLocation).getPath(1);
+            Utils.AssertPolygonsAreEqual(expectedPath, actualPath);
+            
+            // moved left
+            orig = testRects[6]["rect"] as FVRectangle;
+            moved = testRects[6]["movedRect"] as FVRectangle;
+            expectedPath = new Polygon(new Vector2[] {
+                moved.topLeft(),
+                orig.topRight(),
+                orig.bottomRight(),
+                moved.bottomLeft()
+            });
+            actualPath = (testRects[6]["loc"] as RectangleLocation).getPath(1);
+            Utils.AssertPolygonsAreEqual(expectedPath, actualPath);
+
+            // moved down and left
+            orig = testRects[7]["rect"] as FVRectangle;
+            moved = testRects[7]["movedRect"] as FVRectangle;
+            expectedPath = new Polygon(new Vector2[] {
+                orig.topLeft(),
+                orig.topRight(),
+                orig.bottomRight(),
+                moved.bottomRight(),
+                moved.bottomLeft(),
+                moved.topLeft()
+            });
+            actualPath = (testRects[7]["loc"] as RectangleLocation).getPath(1);
+            Utils.AssertPolygonsAreEqual(expectedPath, actualPath);
+        }
+
+        /*
+         * Helpers
+         */
+        public Dictionary<string, object> makeTestRect(Vector2 pos, Vector2 delt, float w, float h)
+        {
+            Dictionary<string, object> rectStuff = new Dictionary<string, object>();
+
+            FVRectangle rect = new FVRectangle(pos, new Vector2(w, h));
+            FVRectangle movedRect = rect + delt;
+
+            RectangleLocation loc = new RectangleLocation(pos, w, h);
+            loc.moveTo(pos + delt);
+
+            rectStuff.Add("loc", loc);
+            rectStuff.Add("rect", rect);
+            rectStuff.Add("movedRect", movedRect);
+            rectStuff.Add("path", null);
+
+            return rectStuff;
+        }
+
+        public Dictionary<string, object>[] starTestRects(Vector2 center, Vector2 delt, float w, float h)
+        {
+            Dictionary<string, object>[] star = new Dictionary<string, object>[8];
+            star[0] = makeTestRect(center, new Vector2(0, delt.Y), w, h);
+            star[1] = makeTestRect(center, new Vector2(delt.X, delt.Y), w, h);
+
+            star[2] = makeTestRect(center, new Vector2(delt.X, 0), w, h);
+            star[3] = makeTestRect(center, new Vector2(delt.X, -delt.Y), w, h);
+
+            star[4] = makeTestRect(center, new Vector2(0, -delt.Y), w, h);
+            star[5] = makeTestRect(center, new Vector2(-delt.X, -delt.Y), w, h);
+
+            star[6] = makeTestRect(center, new Vector2(-delt.X, 0), w, h);
+            star[7] = makeTestRect(center, new Vector2(-delt.X, delt.Y), w, h);
+
+            return star;
+        }
+    }
 }
--- engine/physics_test/utils/Utils.cs
+++ engine/physics_test/utils/Utils.cs
@@ -13,5 +13,74 @@ namespace flatverse.physics.test
             Assert.AreEqual(expected.X, actual.X);
             Assert.AreEqual(expected.Y, actual.Y);
         }
+
+        public static void AssertPolygonsAreEqual(Polygon expected, Polygon actual)
+        {
+            Vector2[] expPts = expected.points();
+            Vector2[] actPts = actual.points();
+
+            for (int i = 0; i < expPts.Length; i++)
+            {
+                if (VectorsAreEqual(actPts[0], expPts[i]) &&
+                    AllPointsEqual(RotateArray<Vector2>(expPts, i), actPts))
+                {
+                    return;
+                }
+            }
+            expPts = ReverseArray<Vector2>(expPts);
+            for (int i = 0; i < expPts.Length; i++)
+            {
+                if (VectorsAreEqual(actPts[0], expPts[i]) &&
+                    AllPointsEqual(RotateArray<Vector2>(expPts, i), actPts))
+                {
+                    return;
+                }
+            }
+
+            Assert.Fail("Polygons not equal");
+        }
+
+        public static T[] RotateArray<T>(T[] array, int startIndex)
+        {
+            T[] rotatedArray = new T[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int ix = (startIndex + i) % array.Length;
+                rotatedArray[i] = array[ix];
+            }
+
+            return rotatedArray;
+        }
+
+        public static T[] ReverseArray<T>(T[] array)
+        {
+            T[] reversedArray = new T[array.Length];
+            int i = reversedArray.Length;
+            foreach (T ele in array)
+            {
+                reversedArray[--i] = ele;
+            }
+
+            return reversedArray;
+        }
+
+        public static bool VectorsAreEqual(Vector2 a, Vector2 b)
+        {
+            return a.X == b.X && a.Y == b.Y;
+        }
+
+        public static bool AllPointsEqual(Vector2[] expected, Vector2[] actual)
+        {
+            for (int i = 0; i < expected.Length; i++)
+            {
+                if (!VectorsAreEqual(expected[i], actual[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
